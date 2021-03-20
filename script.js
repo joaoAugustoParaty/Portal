e9465030bde40e154f39cfd5fcffb1908e3c72b2
João Augusto de Oliveira <joaoaugustowebdev@gmail.com>
2021-03-20 19:27:22 -0300

    Slideshow

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+/*____Primeiro item a ser mostrado___*/
 var slideItem = 0;
+
+/*_____Passar o slide_______*/
 window.onload = function() {
-	setInterval(passarSlide, 2000);
+	setInterval(passarSlide, 2000); 
 
 	var slidewidth = document.getElementById("slideshow").offsetWidth;
 	var objs = document.getElementsByClassName("slide");
@@ -8,21 +11,33 @@ window.onload = function() {
 		objs[i].style.width = slidewidth+"px";
 	}
 }
-function passarSlide() {
-	var slidewidth = document.getElementById("slideshow").offsetWidth;
-	
-	if(slideItem >= 3) {
-		slideItem = 0;
-	} else {
-		slideItem++;
-	}
 
-	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
-}
+/*__________Voltar para o primeiro Slide______*/
+function passarSlide() {
+		var slidewidth = document.getElementById("slideshow").offsetWidth;
+		if(slideItem >= 3) {
+		slideItem = 0;
+		} else {
+		slideItem++;
+		}
+		document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+		var bolinhas = document.getElementsByClassName("bolinha");
+		for (var i = 0; i < bolinhas.length; i++) {
+		bolinhas[i].style.backgroundColor = "#CCC";
+		}
+		bolinhas[slideItem].style.backgroundColor = "#FF0000";
+		}
+
+/*___________EFEITO DE CLICK NAS BOLINHAS_______*/
 function mudarSlide(pos) {
 	slideItem = pos;
-	var slidewidth = document.getElementById("slideshow").offsetWidth;
-	document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+var slidewidth = document.getElementById("slideshow").offsetWidth;
+document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+var bolinhas = document.getElementsByClassName("bolinha");
+for (var i = 0; i < bolinhas.length; i++) {
+bolinhas[i].style.backgroundColor = "#CCC";
+}
+bolinhas[pos].style.backgroundColor = "#FF0000";
 }
 
 function toggleMenu() {
